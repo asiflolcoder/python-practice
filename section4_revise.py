bb111ff8af01ceb2f85f1884496109feaa695437
asiflolcoder <haque.mdasif1501@gmail.com>
2025-08-13 20:30:03 +0600

    section 4 revise codes added

---
+++ section4_revise.py
@@ -0,0 +1,41 @@
+print("operators and expression revise")
+
+#arithmetic operators(+,-,*,/,//,%,**)
+
+# num1 = 20
+# num2 = 10
+# add = num1 + num2
+# sub = num1 - num2
+# multi = num1*num2
+# div = num1/num2
+# print(add)
+# print(sub)
+# print(multi)
+# print(div)
+
+# mod = num1 %num2
+# flo = num1 // num2
+# a,b = 3,5
+# expo = a**b
+# print(expo)
+# print(mod)
+# print(flo)
+
+# a,b,c = 2,3,4
+# print(a**b**c)
+# print(2**81)
+
+#expression - that results in value
+# exp1 = 34+(2*4)/2 - 23 +12
+# print(exp1)
+
+# height = 30
+# base = 10
+# area = 0.5* height* base
+# print(area)
+
+# a = 40
+# a += 20
+# print(a)
+
+
