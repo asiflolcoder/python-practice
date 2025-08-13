56dbebb789c3c7a6fef81269b2a9e3ff9b943506
asiflolcoder <haque.mdasif1501@gmail.com>
2025-08-13 13:05:33 +0600

    section 3 revise codes added

---
+++ section3_revise.py
@@ -0,0 +1,57 @@
+print("Variable and data type revise")
+#section 3 revise - data type 
+#calculate biil amount
+# bills =[120,221,321,95,43]
+# amount =[600,800,1000,400,256]
+# for x in range(len(bills)):
+#   total = bills[x]*amount[i]
+  
+# print (amount)
+# var_name = 234
+
+# num1 = 300
+# num2 = 400
+# sum = num1 +num2 
+# print(sum)
+
+# str_var = "Asif"
+# int_var = 2323234
+# float_var = 234.455
+# bool_var = True
+# print(str_var,type(str_var))
+# print(int_var,type(int_var))
+# print(float_var,type(float_var))
+# print(bool_var,type(bool_var))
+
+# name,age,isStudent = "Asif",24,True
+# print(id(name),id(age),id(isStudent))
+
+# a=b=c=10
+# print(id(a),id(b),id(c))
+
+# var = 234
+# print(var)
+# var = "Asif"
+# print(var)
+# var = False
+# print(var)
+# true = 45
+# false = 33
+
+# complex_var = 34+32j
+# print(complex_var)
+# num = complex(2,3)
+# print(num)
+
+# l = [23,21,"Asif",True]
+# print(l,type(l))
+# for x in l:
+#   print(x)
+# for i in range(len(l)):
+#   print(l[i])
+  
+# l[0]=100
+# l[1] = 50
+# l[2] ="Emon"
+# l[3] = False
+# print(l)
