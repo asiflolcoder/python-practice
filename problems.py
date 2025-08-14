3858ef21a5294accaac4f39bf3716f2c649145cc
asiflolcoder <haque.mdasif1501@gmail.com>
2025-08-14 22:22:34 +0600

    some problems solved

---
+++ problems.py
@@ -0,0 +1,20 @@
+# print("Problems about data type ,variables")
+# name = input("Enter your name:")
+# age = int(input("Enter your age:"))
+# height = float(input("Enter your height:"))
+
+# print(f"Name is {name} and age is {age} and height is {height}")
+
+# fav_foods = ["Pizza","Dragon","Biriyani"]
+# print(fav_foods[0],fav_foods[2])
+
+# str = "Python is awesome"
+# print("length of the string:",len(str))
+
+# apple = int(input("Enter number of apples:"))
+# banana = int(input("Enter number of bananas:"))
+
+# total = 3 * apple + 2 * banana 
+# print(f"total is ${total}")
+
+
