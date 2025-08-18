5adc6f0d24cb22b0325da44941af39e8e2357d2a
asiflolcoder <haque.mdasif1501@gmail.com>
2025-08-18 13:59:50 +0600

    OOP codes added

---
+++ OOP.py
@@ -0,0 +1,17 @@
+print("Hello OOP")
+
+def sum(a,b):
+  return a + b 
+
+print(sum(10,20))
+
+seller1_name = "MD Asif"
+seller1_id = 2323
+seller1_address = '23 street , Bangalore'
+
+seller2_name = "MD Emon"
+seller2_id = 2324
+seller2_address = '24 street , Bangalore'
+
+
+seller1 = ["Asif",2323,"23 street,Bangalore"]
