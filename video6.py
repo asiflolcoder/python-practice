2217069c95afc17af6318298e4a05cc4f5da396d
asiflolcoder <haque.mdasif1501@gmail.com>
2025-08-23 21:07:16 +0600

    day 06 added

---
+++ video6.py
@@ -0,0 +1,38 @@
+print("variable","&","Data type",sep=" ")
+num1 = 20
+print(id(num1))
+counter = 1000
+price = 23.455
+can_vote = False
+
+# print(counter)
+# print(price)
+# print(can_vote)
+# del price
+# print(price)
+#multiple assignment
+# name,age,price = "Asif",24,25.99
+# print(id(name),type(name),name)
+# print(id(age),type(age),age)
+# print(id(price),type(price),price)
+
+# a=b=c = 100
+# print(id(a),id(b),id(c))
+
+#local variable - cannot be accessed outside function
+# def sum():
+#   a=10
+#   b = 20
+#   c = a+b
+#   return c
+
+# print(a,b)
+
+#global variable 
+# x = 10
+# y = 15
+# def sub():
+#   c = x - y
+#   return c
+
+# print(sub())
