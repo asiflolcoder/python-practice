aeb28ea550c3bcb12f7406c84ef9bc1897373b99
asiflolcoder <haque.mdasif1501@gmail.com>
2025-08-25 23:05:42 +0600

    video 14 codes practiced

--- video14.py
+++ video14.py
@@ -23,12 +23,83 @@ print("conditional statement")
 # else:
 #   print("positive")
   
-amount = int(input("Enter amount:"))
-dis = 0
-if (amount>1000):
-  dis = (amount * 10)/100
-  amount_to_pay = amount - dis
-  print("Payment after discount:",amount_to_pay)
-else:
-  print("Discount is not available")
-  print("Payment is",amount)
+# amount = int(input("Enter amount:"))
+# dis = 0
+# if (amount>1000):
+#   dis = (amount * 10)/100
+#   amount_to_pay = amount - dis
+#   print("Payment after discount:",amount_to_pay)
+# else:
+#   print("Discount is not available")
+#   print("Payment is",amount)
+
+# num = int(input("Enter a number:"))
+# if(num<0):
+#   print("negative")
+# elif(num==0):
+#   print("zero")
+# else:
+#   print("positive")
+  
+# amount = int(input("Enter amount:"))
+# discount = 0
+# if(amount>10000):
+#   discount = (amount * 20)/100
+#   amount_to_pay = amount - discount
+#   print("Amount:",amount_to_pay)
+# elif(amount>5000 and amount<=10000):
+#   discount = (amount * 10)/100
+#   amount_to_pay = amount - discount
+#   print("Amount:",amount_to_pay)
+# elif(amount>=1000 and amount <=5000):
+#   discount = (amount * 5)/100
+#   amount_to_pay = amount - discount
+#   print("Amount:",amount_to_pay)
+# elif(amount<1000):
+#   print("no discount")
+# else:
+#   print("input is not valid")
+
+#nested if else
+# num = int(input("Enter a number:"))
+# if num >0:
+#   if num % 2 ==0:
+#     print(num," is even")
+#   else:
+#     print(num,"is odd")
+# else:
+#   if num ==0:
+#     print(num,"is special")
+#   else:
+#     print(num,"is negative")
+
+# import time
+# time = int(time.strftime("%H"))
+# print(time)
+# if (time>6 and time <12):
+#   print("Good Morning")
+# elif time >=12 and time <=16:
+#   print("Good Midnoon")
+# elif time >=16 and time <=20:
+#   print("Good Afternoon")
+# else:
+#   print("Good night")
+  
+# day = "Monday"
+# match day:
+#   case "Sunday":
+#     print(f"Today is {day}")
+#   case "Monday":
+#     print(f"Today is {day}")
+#   case "Tuesday":
+#     print(f"Today is {day}")
+#   case _:
+#     print("Invalid day")
+
+# x = 100
+# match x:
+#   case if x!=10:
+#     print(x," is not 10")
+#   case (x==100):
+#     print(x," is 100")
+
