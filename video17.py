92c4dcd4174d747967ac1d6f7348ec07e7a3edfd
asiflolcoder <haque.mdasif1501@gmail.com>
2025-08-26 14:28:54 +0600

    video 17 codes added

---
+++ video17.py
@@ -0,0 +1,26 @@
+# print("Loops")
+# name = "Md Asif ul Haque"
+# for x in name:
+#   print(x)
+  
+# names = ["Asif","Emon","Hasan","Probin","Rohan"]
+# for x in names:
+#   print(x)
+
+zen = '''
+Beautiful is better than ugly.
+Explicit is better than implicit.
+Simple is better than complex.
+Complex is better than complicated.
+'''
+# numbers = (23,23,11,24,55,23,64,19)
+# total = 0
+# for x in  numbers:
+#   total += x
+
+# print(total)
+
+# numbers = [34,54,67,21,78,97,45,44,80,19]
+# for x in numbers:
+#   if x%2==0:
+#     print(x)
