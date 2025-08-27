a441762f0bd71c81113a373b64555700ce40e5fa
asiflolcoder <haque.mdasif1501@gmail.com>
2025-08-27 19:46:09 +0600

    conditional practice added

---
+++ conditional.py
@@ -0,0 +1,31 @@
+age = 16
+if age>18:
+  print("Eligible to vote")
+else:
+  print("Not eligible to vote")
+
+#elif statement
+age = 34
+if age <=10:
+  print("Child")
+elif age <=18:
+  print("Teenager")
+elif age <=25:
+  print("Young Adult")
+else:
+  print("Adult")
+  
+#nested if else
+num = -4
+if num>0:
+  if num%2==0:
+    print("Even")
+  else:
+    print("Odd")
+else:
+  if num==0:
+    print("Zero")
+  else:
+    print("Negative")
+    
+#calcutor match program
