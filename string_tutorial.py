ec767ac8ad990019bdf5a5bb9550a7a292e75574
asiflolcoder <haque.mdasif1501@gmail.com>
2025-08-29 00:02:24 +0600

    string codes

--- string_tutorial.py
+++ string_tutorial.py
@@ -3,6 +3,51 @@ print("String and methods - Tutorialspoint")
 str1 = 'Hi I am Python'
 str2 = "I don't love Bangladesh"
 str3 = '''I love Python'''
-print(str1,type(str1))
-print(str2,type(str2))
-print(str3,type(str3))
+# print(str1,type(str1))
+# print(str2,type(str2))
+# print(str3,type(str3))
+ 
+# print(str1[0],str1[1])
+# print(str2[1],str2[4])
+
+# for character in str1:
+#   print(character,end=" ")
+# for x in str2:
+#   print(x,end='-')
+
+str1 = "Hello World!"
+print(str1)
+#escape characters
+# print("I am 24 year\'s old")
+# print("Hi i am Asif\nI am 24\nI live in Rajshahi")
+# print("Tab will appear as \tinc")
+# print("Backspace is \bii")
+
+#string slicing
+
+# print("length is :",len(var))
+# print(var[0])
+# print(var[11])
+# print(var[12])
+# print(var[len(var)-3])
+# print(var[len(var)-1])
+# print(var[len(var)-5])
+# print(var[len(var)-12])
+
+# var = "Hello Python"
+# print(var[1:5])
+# print(var[:8])
+# print(var[len(var)-9:len(var)-4])
+
+#string modifying using a list
+# str = "Hello Asif"
+# l = list(str)
+# print(l)
+
+# l[0] ="I"
+# l[1] = " "
+# l[2] = "a"
+# l[3] ="m"
+# print(l)
+# s1 = str(l)
+# print(s1)
