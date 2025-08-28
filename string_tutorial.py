e68e53f60d3fcee33ba4df8e6020e5977060bfed
asiflolcoder <haque.mdasif1501@gmail.com>
2025-08-28 22:53:41 +0600

    string from tutorialspoint added

---
+++ string_tutorial.py
@@ -0,0 +1,8 @@
+print("String and methods - Tutorialspoint")
+
+str1 = 'Hi I am Python'
+str2 = "I don't love Bangladesh"
+str3 = '''I love Python'''
+print(str1,type(str1))
+print(str2,type(str2))
+print(str3,type(str3))
