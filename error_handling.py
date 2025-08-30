c7e4ffb8792d4e63185eb6156a4f78533d925edb
asiflolcoder <haque.mdasif1501@gmail.com>
2025-08-30 12:02:28 +0600

    error handling codes

---
+++ error_handling.py
@@ -0,0 +1,18 @@
+print("Error handling")
+# num1 = 100
+# num2 = 0
+# div = num1/num2
+# print("some important line of code")
+# print("nfjanfaj")
+# num = 10
+# print num
+
+# ab
+# if a
+# print(a)
+
+# print(1 + 'a')
+# d = {"key1":"value1"}
+# d["key2"]
+
+# import mat
