81ddd6890dc8147150aeac64726c96f7dc7f90b6
asiflolcoder <haque.mdasif1501@gmail.com>
2025-08-30 12:03:55 +0600

    working tree clean

--- video17.py
+++ video17.py
@@ -24,3 +24,20 @@ Complex is better than complicated.
 # for x in numbers:
 #   if x%2==0:
 #     print(x)
+
+# for i in range(0,5,1):
+#   print(i)
+# for num in range(10):
+#   print(num, end=" ")
+  
+# for num in range(10,20):
+#   print(num, end=" ")
+
+# for num in range(1,10,2):
+#   print(num,end="-")
+
+# dic = {"name":"Asif","age":24,"is_student":False}
+# for key in dic:
+#   print(key,":",dic[key])
+
+#for else loop
