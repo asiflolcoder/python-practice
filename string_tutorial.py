bd81e6c93c508b29b7da29016a24fc8214cb2768
asiflolcoder <haque.mdasif1501@gmail.com>
2025-08-30 12:01:53 +0600

    string codes

--- string_tutorial.py
+++ string_tutorial.py
@@ -50,4 +50,39 @@ print(str1)
 # l[3] ="m"
 # print(l)
 # s1 = str(l)
-# print(s1)
+# print(s1)\
+
+# s1 = "WORD"
+# print("Original string:",s1)
+# li = list(s1)
+# li.insert(3,"L")
+# print(li)
+# s1 =""
+# s2=s1.join(li)
+# print(s2)
+
+# import array as ar
+# s1 = 
+
+#string concatenation
+f_name = "Md Asif"
+l_name = "ul Hauqe"
+print(f_name+" "+l_name)
+
+str1 = "abc"
+print(str1*10)
+# print("Asif"+23)
+
+str1 ="Hello"
+str2 = "World"
+str3 = str1+" "+str2
+print(str3)
+
+#string repetition
+h1 = "Hello"
+print(h1 *3 )
+
+s1 = "I love"
+s2 = "Python"
+concat = s1 + " " + s2
+print(concat * 3)
