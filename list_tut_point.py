e2b06450281aa9efdd41b6c39c4714aa4e8dd6df
asiflolcoder <haque.mdasif1501@gmail.com>
2025-09-01 03:45:20 +0600

    list codes added

---
+++ list_tut_point.py
@@ -0,0 +1,47 @@
+print("Everything about list from Tutorialspoint")
+
+# list1 = [12,32,1,2,3,4]
+# list2 = ["Asif",True,23.45,2+3j]
+# print(list1,list2)
+
+# print(list1[0])
+# print(list1[2])
+# print(list2[3])
+
+# list1[0] = 100
+# list1[1] = 50
+# print(list1)
+
+# list2[0] ="Emon"
+# list2[3] = 3+4j
+# print(list2)
+
+# del list2[0]
+# del list2[1]
+# print(list2)
+
+# #list concatenation(+)
+# l1 = [1,2,3]
+# l2 = [4,5,6]
+# l3 = l1 + l2
+# print(l3)
+
+# #list repitition
+# l1 = [12,23]
+# print(l1*4)
+
+# print(3 in [1,3,4])
+
+#accessing list iteams
+l1 = ["a","b","c","d","e"]
+print(l1[len(l1)-3])
+print(l1[-2])
+
+#slie operator
+print(l1[1:3])
+l2 = [12,23,22,11,44,24,4]
+print(l2[0:5:2])
+print(l2[1:3])
+
+#change list
+
