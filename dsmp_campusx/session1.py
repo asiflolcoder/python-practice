3b90e4d5eaeb49f028ba0e83deb236b7949df7f8
asiflolcoder <haque.mdasif1501@gmail.com>
2025-09-04 19:08:39 +0600

    some

---
+++ dsmp_campusx/session1.py
@@ -0,0 +1,45 @@
+print("Data Science Mentorship Program - Class 1")
+
+# print("Hello Bro",23,12.33)
+# print(12,22,sep='')
+# print("Asif",23,end='-')
+
+# print("Asif","Emon","Hasan",sep='-',end='\'')
+
+#data types
+# import sys
+# int_var = 23211
+# float_var = 23.22312
+# bool_var = True
+# print(sys.getsizeof(int_var),sys.getsizeof(float_var))
+
+# li = [12,"Asif",True,23.99]
+# tu = (23,12,"Emon",False)
+# str1 ="Hello Python"
+
+# print(int_var,type(int_var))
+# print(float_var,type(float_var))
+# print(bool_var,type(bool_var))
+# print(li,type(li))
+# print(tu,type(tu))
+# print(str1,type(str1))
+
+# set1 ={23,22,11,22,43}
+# dict1 = {"name":"Asif","age":24}
+# null = None
+# print(set1,type(set1))
+# print(dict1,type(dict1))
+# print(null,type(null))
+
+a = 323
+print(a)
+a ="Asif"
+print(a)
+
+a,b,c = 12,"Asif",24
+p=q=r =45
+
+num1 = int(input("Enter a number:"))
+num2 = int(input("Enter another number:"))
+sub = num1 - num2
+print(sub)
