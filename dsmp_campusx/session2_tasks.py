33647db4a94bb3e7810f3f7d01781b10e1a57c52
asiflolcoder <haque.mdasif1501@gmail.com>
2025-09-07 22:22:17 +0600

    session 2 codes added

---
+++ dsmp_campusx/session2_tasks.py
@@ -0,0 +1,37 @@
+#question 1
+# ctc = int(input("Enter CTC:"))
+# hra = ctc * 0.1
+# da = ctc * 0.05
+# pf = ctc * 0.03
+
+# salary = ctc - (hra + da + pf)
+
+
+# if salary < 5:
+#   tax = ctc * 0.0
+#   salary = (salary - tax)/12
+# elif salary >=5 and salary < 10:
+#   tax = ctc * 0.1
+#   salary = (salary - tax)/12
+# elif salary >=10 and salary<20:
+#   tax = ctc * 0.2
+#   salary = (salary - tax)/12
+# else:
+#   tax = ctc * 0.3
+#   salary = (salary - tax)/12
+  
+# print(salary * 100000)
+
+#question 2
+# angle1 = float(input("Enter a angle:"))
+# angle2 = float(input("Enter a angle:"))
+# angle3 = float(input("Enter a angle:"))
+
+# sum_of_angles = angle1 + angle2 + angle3
+
+# if sum_of_angles ==180.0 and (angle1 >0 and angle2 >0 and angle3 >0) :
+#   print("Can form a triangle")
+# else:
+#   print("Cannot form a triangle")
+
+#question 3
