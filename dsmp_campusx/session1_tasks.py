c08fa0623d5643577a8591a1c3df40b6f9e906c5
asiflolcoder <haque.mdasif1501@gmail.com>
2025-09-07 22:21:19 +0600

    codes

--- dsmp_campusx/session1_tasks.py
+++ dsmp_campusx/session1_tasks.py
@@ -49,3 +49,38 @@ print("Data","Science","Mentorship","Program","Started","By","CampusX",sep="-",e
 # print("interest is:",interest)
 
 
+# Q6:- Write a program that will tell the number of dogs and chicken are there when the user will provide the value of total heads and legs.
+#input from user
+# head = int(input("Enter amount of heads:"))
+# leg = int(input("Enter amount of legs:"))
+# #calculation
+# dog = 
+
+# Q7:- Write a program to find the sum of squares of first n natural numbers where n will be provided by the user.
+#input from user
+# num = int(input("Enter a number:"))
+# sum = 0
+# for i in range(1,num+1):
+#   sum = sum + (i*i)
+
+# # print("Sum is:",sum)
+# sum = (num * (num + 1) * (2*num + 1))/6
+# print("sum is:",sum)
+
+# Q8:- Given the first 2 terms of an Arithmetic Series.Find the Nth term of the series. Assume all inputs are provided by the user.
+# num1 = int(input("Enter 1st number:"))
+# num2 = int(input("Enter 2nd number:"))
+# diff = num2 - num1
+# n = int(input("nth term:"))
+
+# l = num1 + (n - 1)* diff 
+# print("n th term:",l)
+
+num1 = int(input("Enter numerator:"))
+den1 = int(input("Enter a denominator:"))
+
+num2 = int(input("Enter numerator:"))
+den2 = int(input("Enter a denominator:"))
+
+sum = (num1 * den2 + num2 * den1)/(den1 * den2)
+print("Sum of two fraction:",sum)
