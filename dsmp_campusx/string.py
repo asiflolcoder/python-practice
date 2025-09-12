5b04596de448598e0dfc6d3cb45bd29e8a4b3806
asiflolcoder <haque.mdasif1501@gmail.com>
2025-09-12 21:06:37 +0600

    string codes added

---
+++ dsmp_campusx/string.py
@@ -0,0 +1,56 @@
+# import sys
+# str = "Hello Python"
+# print(sys.getsizeof(str))
+
+#creating string 
+# str1 = 'Hello Asif'
+# str2 = "Hello Probin"
+# str3 = '''Hello Hasan'''
+# print(str1,str2,str3)
+
+# print("It's raining")
+# print('Double " quotes')
+# multi_string = """
+#     Hi 
+#    What do you want?
+# """
+# print(multi_string)
+
+# str1 = str("Here is a string")
+# print(str1)
+# print(str([12,32,11]))
+
+#accessing substrings
+s1 = "hello world"
+# print(s1[1])
+# print(s1[2])
+# s1[20]
+# print(s1[len(s1)-5])
+
+#slicing
+# print(s1[::1])
+# print(s1[1:4])
+# print(s1[5:])
+# print(s1[1::2])
+# print(s1[::-1])
+# print(s1[-1:-4:-1])
+# print(s1[-8:-4])
+# print(s1[-3:-1])
+
+#edit and modify a string
+# str1 = "I love Python"
+# # str[0] ="W"
+# # str1 = str[0]
+# print(str1)
+# del str1
+# print(str1)
+# str1 = "I love Python"
+# # del str[0:5]
+# li = list(str1)
+# print(li)
+# li[0:2] ="We"
+# print(li)
+# s1 = ''.join(li)
+# print(s1)
+
+
