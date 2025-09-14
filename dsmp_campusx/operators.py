c815640c380bd669e41e79ccce1b036d418368b6
asiflolcoder <haque.mdasif1501@gmail.com>
2025-09-14 06:27:51 +0600

    operator codes practiced

---
+++ dsmp_campusx/operators.py
@@ -0,0 +1,62 @@
+print("Operators and operations")
+#arithmetic operators
+# print(5 + 6)
+# print(10-6)
+# print(10*3)
+# print(30/4)
+# print(40//6)
+# print(50 % 7)
+# print(12**3)
+
+#operator precedence (),**,* / // %,+ -
+# print(23*(4+3)/7 - 2**3 )
+
+#relational/comparison operator(>,>=,<,<=,==,!=)
+
+# print(5 > 4)
+# print(51 >=53)
+# print(3<4)
+# print(5<=2)
+# print(55==44)
+# print(12!=32)
+
+#logical operator(and,or,not)
+# print(True and True)
+# print(True or False)
+# print(not True)
+
+# print('' and 34)
+# print("HEre is a")
+
+# print(23 or 43)
+# print('' or 22)
+
+# print(not '')
+#assignment operator(=,+=,-=,*=,/=,%=)
+
+# a = 10
+# b = 5
+# a+=b
+# print(a)
+# b-=a
+# print(b)
+# a*=b
+# print(a)
+# a/=b
+# print(a)
+# print(b)
+# a = 23
+# b = 4
+# a//=b
+# print(a)
+# a%=b
+# print(a)
+
+#identity operator
+# print(10 is 30)
+# print(5 is not 10)
+
+#membership operator
+# print(4 in [23,4,32,1])
+# print("c" in "Cat")
+# print("e" not in "Eagly")
