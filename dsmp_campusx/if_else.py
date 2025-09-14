2cdafca2aa4ef27ea4d3b9cddea86d4e2cb0a0ec
asiflolcoder <haque.mdasif1501@gmail.com>
2025-09-14 07:43:18 +0600

    control flow codes added

---
+++ dsmp_campusx/if_else.py
@@ -0,0 +1,54 @@
+# print("Control flow")
+
+# email = input("Enter your email:")
+# password = input("Enter your password:")
+
+# if email =="abc@gmail.com" and password == "1234":
+#   print("login successful")
+# elif email == "abc@gmail.com" and password != "1234":
+#   print("Incorrect password")
+#   password = input("Enter password again:")
+#   if password =="1234":
+#     print("Welcpme finally")
+#   else:
+#     print("Tumse nahi ho payega")
+# else:
+#   print("Login failed")
+
+# age = 25
+# if age<=12:
+#   print("Child")
+# elif age <=19:
+#   print("Teenger")
+# elif age <=24:
+#   print("Young Adult")
+# else:
+#   print("Adult")
+
+#max of three numbers
+# num1 = int(input("Enter a number:"))
+# num2 = int(input("Enter a number:"))
+# num3 = int(input("Enter a number:"))
+# if num1 >num2 and num1 > num3:
+#   print(num1," is greatest")
+# elif num2>num3:
+#   print(num2," is greatest")
+# else:
+#   print(num3," is greatest")
+
+#match case
+# num1 = float(input("Enter a number:"))
+# num2 = float(input("Enter a number:"))
+# op = input("Enter an operator:")
+# match op:
+#   case '+':
+#     print(num1+num2)
+#   case '-':
+#     print(num1 -num2)
+#   case '*':
+#     print(num1*num2)
+#   case '/':
+#     print(num1/num2)
+#   case _:
+#     print("Operation not valid")
+  
--- dsmp_campusx/operators.py
+++ dsmp_campusx/operators.py
@@ -62,10 +62,10 @@ print("Operators and operations")
 # print("e" not in "Eagly")
 
 # Program - Find the sum of a 3 digit number entered by the user
-num = int(input("Enter a 3 digit number:"))
-a = num % 10 
-num = num // 10
-b = num % 10
-num = num // 10
-c = num % 10
-print("sum is :",a+b+c)
+# num = int(input("Enter a 3 digit number:"))
+# a = num % 10 
+# num = num // 10
+# b = num % 10
+# num = num // 10
+# c = num % 10
+# print("sum is :",a+b+c)
