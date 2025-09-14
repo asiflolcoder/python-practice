431bc0efb4c22573e561b467dd038de69f61310e
asiflolcoder <haque.mdasif1501@gmail.com>
2025-09-14 06:32:26 +0600

    codes

--- dsmp_campusx/operators.py
+++ dsmp_campusx/operators.py
@@ -60,3 +60,12 @@ print("Operators and operations")
 # print(4 in [23,4,32,1])
 # print("c" in "Cat")
 # print("e" not in "Eagly")
+
+# Program - Find the sum of a 3 digit number entered by the user
+num = int(input("Enter a 3 digit number:"))
+a = num % 10 
+num = num // 10
+b = num % 10
+num = num // 10
+c = num % 10
+print("sum is :",a+b+c)
