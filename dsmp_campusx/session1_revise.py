31c487fb3aa8448cfe00ca25daa7f0a06fbc4258
asiflolcoder <haque.mdasif1501@gmail.com>
2025-10-10 12:50:20 +0600

    session 1 revise

---
+++ dsmp_campusx/session1_revise.py
@@ -0,0 +1,49 @@
+import sys
+print("Session 1 revise")
+
+# print("Hello World")
+# print("Hello","I am",24)
+# print("Asif",False,23.4,sep='-')
+# print("Asif",False,23.4,sep=' ')
+# print("Asif",False,23.4,sep=' ',end='\n')
+# print("session1",end='/')
+
+#numeric type
+# num1 = 234
+# num2 = 342425252242424
+# print(sys.getsizeof(num1))
+# print(sys.getsizeof(num2))
+# print(type(num1),type(num2))
+
+# flo1 = 231.2323
+# flo2 = 3424424241.242424242
+# print(sys.getsizeof(flo1))
+# print(sys.getsizeof(flo2))
+# print(type(flo1),type(flo2))
+# bool1 = True
+# bool2 = False
+# print(sys.getsizeof(bool1))
+# print(type(bool2))
+
+# complex1 = 3 + 4j
+# print(sys.getsizeof(complex1))
+# num1 = complex(3,4)
+# print(num1)
+
+#sequence type - list,tuple,string
+# li = [23,"Asif",True]
+# print(li,type(li))
+# str = "hello python"
+# print(str,type(str))
+
+# tu = (23,22,11,"Asif")
+# print(tu,type(tu))
+
+# set1 = {23,"Asif",True}
+# print(set1,type(set1))
+
+# dict1 = {"name":"Asif"}
+# print(dict1,type(dict1))
+
+# num1 = None
+# print(num1,type(num1))
