82b3bae14d8e6ec9e5daef7b61fd57372f297568
asiflolcoder <haque.mdasif1501@gmail.com>
2025-10-11 14:14:43 +0600

    practice finished

--- dsmp_campusx/session1_revise.py
+++ dsmp_campusx/session1_revise.py
@@ -47,3 +47,81 @@ print("Session 1 revise")
 
 # num1 = None
 # print(num1,type(num1))
+
+#dynamic binding
+# a = 3422
+# print(a)
+# a = "Asif"
+# print(a)
+# b,c,d = 23,22,1
+# print(b,c,d)
+# a=b=c= 100
+# print(id(a),id(b),id(c))
+
+#keywords and identifier
+# 1ab = 23
+# ab1 = 23
+# ab_23 = 2322
+
+# # input1 = input("")
+
+# input1 = int(input("Enter a number:"))
+# input2 = int(input("Enter a number:"))
+# sum = input1 + input2
+# print(sum)
+
+
+#type conversion
+# print(10+10.5) #implicit
+# num1 = int("23")
+# print(num1)
+# print(34+"34")
+
+#literals
+# a = 2 
+# print(a)
+# a = 0b1010
+# print(a)
+# a= 0o423
+# print(a)
+# a = 0x12c
+# print(a)
+
+# a = 22e10
+# print(a)
+
+# f = 12.342
+# print(f)
+# f = 232.23e2
+# print(f)
+# f = 211.e-3
+# print(f)
+
+# str1 = 'Hello Python'
+# print(str1)
+# str1 ="Hello Bro"
+# print(str1)
+# str1 = """Triple quotes"""
+# print(str1)
+
+# print("escape characters\n \\ \'")
+
+#complex literals
+# complex1 = 3 + 4j
+# complex2 = complex(4,3)
+# print(complex1,complex2)
+
+#boolean
+# bool1 = True
+# bool2 = False
+# print(bool1,int(bool1))
+# print(bool2,int(bool2))
+
+# a = True + 1
+# print(a)
+
+# b = False - 0
+# print(b)
+
+# a = None
+# print(a)
