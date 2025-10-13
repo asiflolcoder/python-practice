3b5c7a20ff7d35b3c78925feb0a8b53d514c865e
asiflolcoder <haque.mdasif1501@gmail.com>
2025-10-14 01:35:08 +0600

    session 2 codes added

---
+++ dsmp_campusx/session2_revise.py
@@ -0,0 +1,69 @@
+print("Operators and control flow")
+
+#arithmetic(+,-,*,/,//,%,**)
+# num1 = 24
+# num2 = 11
+# add = num1 + num2
+# sub = num1 - num2
+# multi = num1 * num2
+# div = num1 / num2
+# int_div = num1 //num2
+# mod = num1 %num2
+# print(add)
+# print(sub)
+# print(multi)
+# print(div)
+# print(int_div)
+# print(mod)
+
+# a ,b = 3,4
+# print(a**b)
+
+#precedence of arithmetic operator-(),**(RL) // % /*,+,-
+# a = 3 ** 4 ** 3
+# # print(a)
+# b = (3+4)*4 +4-3*3
+# print(b)
+
+#comparison operator - (>,>=,<,<=,==,!=)-returns boolean
+# print(4>5)
+# print(23<45)
+# print(55>=55)
+# print(44<=23)
+# print(44==34)
+# print(34!=34)
+
+#is vs ==
+# print(12==23)
+# print(12 is 12)
+
+# l1 = [1,2,3]
+# l2 = [1,2,3]
+# print(l1 == l2)
+# print(l1 is l2)
+
+#logical operator
+# print(True and False)
+# print(True and True)
+# print(False and True)
+# print(False and False)
+# print(12 and "Asif")
+# print(0 and "Probin")
+# print(False and [1,2,3])
+
+# print(True or False)
+# print(True or True)
+# print(False or True)
+# print(False or False)
+# print("Asif" or 12)
+# print(0 or "Asif")
+
+# print(23 and "Asif") #Asif
+# print(0 or 2) # 2
+# print(0 and True) # 0
+# print("Asif" or False) #Asif
+
+# print(not 23)
+# print(not False)
+
+#bitwise operator - performs on bits
