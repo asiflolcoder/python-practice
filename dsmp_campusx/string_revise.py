4be6015d5b795f236951681f700cf91958e70fbb
asiflolcoder <haque.mdasif1501@gmail.com>
2025-10-13 21:51:40 +0600

    string revise codes added

---
+++ dsmp_campusx/string_revise.py
@@ -0,0 +1,61 @@
+print("String and methodss")
+#string creation
+# str1 = 'Hello Python'
+# str2 = "My name is Asif"
+# str3 = """Tripple quoted strings
+# Hi I am Someone"""
+# print(str1,str2,str3,end='\n',sep='\n')
+# print('I am 24 year\"s old')
+# num1 = 24
+# s1 = str(num1)
+# print(s1,type(s1))
+
+#accessing characters in a string
+
+# print(str1[1])
+# print(str1[0])
+# print(str1[2])
+# print(str1[4])
+# print(str1[20])
+# print(str1[-1])
+# print(str1[len(str1)-4])
+# print(str1[-4])
+# print(str1[len(str1)-6])
+
+str1 = "Hello Python"
+#string slicing [::]
+# print(str1[:])
+# print(str1[1:5])
+# print(str1[3:])
+# print(str1[1:5:2])
+# print(str1[::-1])
+# print(str1[-5:])
+
+#string is immutable - particular value change kora jai na ,notun string toiri hoy
+# s1 = str1[1:4]
+# print(s1)
+# str[1:4] ="Asi"
+# for x in str1:
+#   print(x)
+str1 ="Hello Bangladesh"
+# for i in range(len(str1)):
+#   print(str1[i])
+print(str1)
+# del str1[1:4]
+# del str1
+# print(str1)
+
+#update a string
+# s = "Aello Asif"
+# s1 = "H"+s[1:]
+# print(s1)
+# s2 = s.replace()
+
+#operators in string - concatenation(+),multiplication(*),membership 
+# print("MD Asif"+" "+"ul Haque")
+# age = 24
+# # print("I am"+years old")
+# print("Hi"*5)
+
+# print("Hi" in "Hi Bangladesh")
+
