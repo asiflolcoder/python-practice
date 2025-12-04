3a3fb6e3bfc4d93580956217e0203737c51d77b9
asiflolcoder <haque.mdasif1501@gmail.com>
2025-12-04 12:11:58 +0600

    string codes added

---
+++ strings.py
@@ -0,0 +1,42 @@
+#creating string
+# print('Hello World')
+# print("Hello Python")
+# print("""I am asif""")
+# print("I am 24 year's old")
+# print('I am " Asif')
+# print("""Hello 
+#       My name is
+#       Asif""")
+# print(str(23))
+# li = str([12,3,2])
+# print(li,type(li))
+
+#accessing the string - indexing/slicing
+s1 = "Hello Bangladesh"
+# print(s1[0])
+# print(s1[4])
+# print(s1[-2])
+# print(s1[len(s1)-5])
+# print(s1[::])
+# print(s1[::-1])
+# print(s1[2:6:2])
+# print(s1[:5])
+# print(s1[-1:-5:-1])
+# print(s1[-5:])
+# print(s1[-7:-3])
+
+#edit and delete a string
+#string is immutable , can't change particular value
+s1 = "Hello Python"
+# s1[0] ="A"
+# print(s1)
+# s2 = s1[:5]
+# print(s2)
+# s3 = s1[0]
+# print(s3)
+# del s1[0]
+# del s1[:5]
+# del s1
+# print(s1)
+
+
