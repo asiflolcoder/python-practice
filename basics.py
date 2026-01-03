26f0e6226e0e0f7447c21d17b7e22325d0fca0ba
asiflolcoder <haque.mdasif1501@gmail.com>
2026-01-03 22:38:40 +0600

    basics revised

---
+++ basics.py
@@ -0,0 +1,68 @@
+print("Hello Basics From python")
+print("asif",24,sep='-',end='\n')
+print("Hello","World",24,True,end='',sep='')
+
+#single line comment
+"""High
+This is a 
+multi line
+comment"""
+import sys
+#data types - numeric(int,float,boolean,complex)
+# int_var = 24433432849486984
+# print(int_var,type(int_var))
+# print(sys.getsizeof(int_var))
+
+# float_var = 12342242242242.24225223243224424
+# print(float_var,type(float_var))
+# print(sys.getsizeof(float_var))
+
+# bool1 = True
+# bool2 = False
+
+# print(bool1,type(bool1))
+# print(sys.getsizeof(bool1))
+# print(bool2,type(bool2))
+# print(sys.getsizeof(bool2))
+
+# complex1 = 2 + 3j
+# complex2 = complex(2,3)
+# print(complex1,type(complex1))
+# print(sys.getsizeof(complex1))
+
+#sequence typed - string,list,tuple
+# str1 = "Hello Asif"
+# print(str1,type(str1))
+# str2 = str(23)
+# print(str2,type(str2))
+# print("\n")
+# l1 = [12,3,342,42]
+# t1 = (12,32,1132,23)
+# print(l1,type(l1))
+# print(t1,type(t1))
+
+# s = {12,3,22,324,2}
+# print(s,type(s))
+
+# dict1 = {"name":"Asif","age":24}
+# print(dict1,type(dict1))
+
+# a = None
+# print(a,type(a))
+
+# a = 23
+# print(a)
+# a = "Asif"
+# print(a)
+# a = True
+# print(a)
+
+# a,b,c = 23,34,22
+# print(a,b,c)
+
+# #keywords - for,if,else
+
+# a = b = c = 100
+# print(a,b,c)
+# print(id(a),id(b),id(c))
+
