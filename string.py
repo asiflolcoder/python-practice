5e4173d2e75e8ed2a97641b22894a6e10f3f39ab
asiflolcoder <haque.mdasif1501@gmail.com>
2026-01-11 18:28:48 +0600

    strings code added

--- string.py
+++ string.py
@@ -22,3 +22,57 @@ s = "Hello Python"
 # print(s[15])
 
 #slicing
+# print(s[::])
+# print(s[::-1])
+# print(s[0:6:2])
+# print(s[1:5])
+# print(s[-1:-5:-1])
+# print(s[-1::])
+# print(s[-6:])
+# print(s[-5:-1])
+
+#editing and deleting strings
+# s[0] ="A"
+# s1 = s[0]
+# print(s1)
+# s2 = s[1:4]
+# print(s2)
+# del s[0]
+# del s[0:5]
+# del s
+# print(s)
+
+#operations on string
+# print("Asif"+" "+"Arfi")
+# print("Delhi"+ "Mumbai")
+# print("Asif"*3)
+# print("Asif"-23)
+# print("*"*50)
+# print("Asif">"Cinema")
+# print("Emon">)
+
+#relational operator
+# print("Asif"=="Asif")
+# print(12 == 12.00)
+# print("Dhaka"!="dhaka")
+# print("Asif">"Asif")
+# print("Asif"<"Emon")
+# print('qwer'>="1234")
+
+# print("Asif" and "Afiya")
+# print("" and "A")
+
+# print("Asif" or 12)
+# print(False and "Asif")
+
+# print(not "")
+# print(not "Asif")
+
+# for i in "Hello World":
+#   print(i)
+  
+# s = "Asif is 24 years old"
+# for i in range(0,len(s)):
+#   print(s[i])
+
+#functions-len,min,max,sorted
