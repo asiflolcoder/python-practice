39325e1849d890c066a9d8b5ad7829f824dc2d4d
asiflolcoder <haque.mdasif1501@gmail.com>
2026-01-11 13:19:29 +0600

    string codes added

---
+++ string.py
@@ -0,0 +1,24 @@
+print("String and methods")
+
+# s1 = 'Hello Asif'
+# s2 = "Hello Emon"
+# s3 = """I am 
+#       24 years old"""
+
+# print(s1,s2,s3)
+# s4 = str(2443.4)
+# print(s4,type(s4))
+
+# print("Hi i am 30 year's old")
+# print('Hlw How are "you"')
+
+#accessing stings-indexing.slicing
+s = "Hello Python"
+# print(s[0])
+# print(s[6])
+# print(s[-1])
+# print(s[len(s)-6])
+
+# print(s[15])
+
+#slicing
