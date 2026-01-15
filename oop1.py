3893855229a8c64923de3f3df992fc31b100aef8
asiflolcoder <haque.mdasif1501@gmail.com>
2026-01-15 11:24:14 +0600

    oop codes added

---
+++ oop1.py
@@ -0,0 +1,41 @@
+print("Python object oriented programming")
+
+# l = [1,2,3]
+# l.upper()
+
+#oop - class ,object,encapsulation,inheritance,polymorphism,abstraction
+
+# l = [1,2,3]
+# print(type(l))
+
+#class - property/attribute - blueprint of object
+# class Student:
+#   def __init__(self)
+
+# st1 = Student()
+# class Human:
+#   def __init__(self):
+#     print("Human object created")
+    
+# asif = Human()
+
+# class Student:
+#   def __init__(self):
+#     self.name = "Asif"
+#     self.age = 25
+#     self.course ="CSE"
+#     print(f"name:{self.name} age:{self.age} course:{self.course}")
+
+# stu_asif = Student()
+
+# class Student:
+#   def __init__(self,name,age,course):
+#     self.name = name
+#     self.age = age
+#     self.course = course
+#     print(f"name:{self.name} age:{self.age} course:{self.course}")
+
+# student1 = Student("Probin",24,"EEE")
+# student2 = Student("Asif",25,"CSE")
+# print(type(student1))
+    
