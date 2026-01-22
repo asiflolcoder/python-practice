515c77ed48fc51b45c1fb48fa482616657b27a34
asiflolcoder <haque.mdasif1501@gmail.com>
2026-01-22 12:07:13 +0600

    some codes added

--- list.py
+++ list.py
@@ -40,3 +40,45 @@ print("List and methods")
 # print(l1 is l2)
 
 #creating a list
+# l1 = []
+# print(l1)
+# l2 = ["Asif","Probin","Hasan"]
+# print(l2)
+# l3 = [12,34,[1,2],[2,3]]
+# print(l3)
+# print(l3[2][0])
+# l4 = ["Asif",24,True]
+# print(l4)
+# l5 = [[1,2,[5,6]],[0,10,[20,30]]]
+# print(l5[0][2][0],l5[1][2][1])
+# li = list("Asif")
+# print(li)
+# l1 = list((12,34,55))
+# print(l1)
+# l2 = list([12,34,[1,2]])
+# print(l2)
+#accessing items from a list
+l = [10,20,30,40,50]
+# print(l[0])
+# print(l[3])
+# print(l[-1])
+# print(l[len(l)-4])
+# print(l[::])
+# print(l[::-1])
+# print(l[1:4])
+# print(l[0:3])
+# print(l[-5:-1:])
+
+#adding items on a list-append,insert,extend
+# l = [10,20,30,40,50,60]
+# print(l.append(70))
+# print(l)
+# l.extend("Asif")
+# print(l)
+# l.extend([1,2,3])
+# print(l)
+# l.insert(0,'Asif')
+# print(l)
+
+
+
