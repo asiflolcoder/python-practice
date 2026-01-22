0e4a2553d4ea795b62ced9fc44d6541595e0725f
asiflolcoder <haque.mdasif1501@gmail.com>
2026-01-22 11:34:12 +0600

    list codes added

---
+++ list.py
@@ -0,0 +1,42 @@
+print("List and methods")
+
+# l = [10,"Asif",25.99,True]
+# print(id(l[0]))
+# print(id(l[1]))
+# print(id(l[2]))
+# print(id(l[3]))
+
+#characteristics of list
+# l = [1,2,3,4]
+# print(l[1])
+# print(l[-1])
+# print(l[0])
+
+# l = [12,34,22]
+# l[0] = 10
+# print(l)
+
+# l = [12,"Asif",23.44]
+# print(l)
+
+# l = [12,34,12]
+# print(l)
+# l.append(100)
+# print(l)
+# l = [12,10,[12,12]]
+# print(l)
+
+# l = [(1,2),"Asif",{"name":"Asif"}]
+# print(l)
+
+# ulti_list = [10,[20,30],"Asif",True,(5,6),{"age":24}]
+# print(ulti_list[0])
+# print(ulti_list[-1])
+# ulti_list[0] = 100
+# print(ulti_list)
+# l1 = [12,3,43]
+# l2 = [12,3,43]
+# print(l1 == l2)
+# print(l1 is l2)
+
+#creating a list
