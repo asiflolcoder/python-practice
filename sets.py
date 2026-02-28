3684bcef1a169eb3f3c1002175ae1d82f461bf88
asiflolcoder <haque.mdasif1501@gmail.com>
2026-02-28 15:27:07 +0600

    sets added

---
+++ sets.py
@@ -0,0 +1,32 @@
+print("sets")
+#s1 = {12,34,10,"Asif",True}
+#print(s1)
+# print(s1)
+# s1 = set()
+# print(s1)
+
+#creating sets
+s1 = {}
+# print(s1,type(s1))
+s1 = set()
+#print(s1,type(s1))
+s2 = {12,3,23,12}
+# print(s2)
+#s3 = {1,2,3,4,{1,2}}
+s4 = {12,3,"Asif",True,(1,2,3)}
+print(s4)
+
+# s5 = set([2,3,4,4,6433,3,53535,3])
+# s6 = set("Asif")
+# print(s5,s6)
+
+# s1 = {1,2,3}
+# s2 = {3,2,1}
+# print(s1==s2)
+
+#accessing items
+s1 = {"Asif",24,True,23.99}
+# print(s1[0])
+# print(s1[0:3])
+# s1[0] = 10
+
